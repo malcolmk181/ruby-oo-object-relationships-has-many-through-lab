b80446f28c6d67f9c11fed16a4f27e9a98f5024e
Malcolm Keyes <3.malcolmk@gmail.com>
2022-06-01 13:50:02 -0400

    commit tree a3cf39aa301a581c9f88ed55c116ee983d88afb5

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,22 @@
+class Genre
+    attr_accessor :name
 
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    def initialize(name)
+        @name = name
+        self.class.all << self
+    end
+
+    def songs
+        Song.all.select {|song| song.genre == self }
+    end
+
+    def artists
+        songs.collect {|song| song.artist }
+    end
+end
